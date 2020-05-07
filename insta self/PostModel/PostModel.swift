1da40a541a6f0139f0b63fef5673b66a28e834a3
ASW-研修３ <training03@ASW-kenshuu3noMacBook-Air.local>
2020-05-07 15:27:47 +0900

    readsまで

---
+++ insta self/PostModel/PostModel.swift
@@ -0,0 +1,30 @@
+//
+//  PostModel.swift
+//  insta self
+//
+//  Created by ASW-研修３ on 2020/05/07.
+//  Copyright © 2020 ASW-研修３. All rights reserved.
+//
+
+import UIKit
+
+class PostModel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
